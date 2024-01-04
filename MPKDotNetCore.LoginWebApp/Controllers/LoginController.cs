b51d95981bcdef87bd6c38780c19d85122d9f856
myapwintkhine <myapwintkhine@acedatasystems.com>
2024-01-04 14:53:03 -0800

    [Complete] Login Web App

---
+++ MPKDotNetCore.LoginWebApp/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MPKDotNetCore.LoginWebApp.Models;
+using Newtonsoft.Json;
+
+namespace MPKDotNetCore.LoginWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            var str = HttpContext.Session.GetString("LoginData");
+            if (str is not null) return Redirect("/");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel requestModel)
+        {
+            // logic
+
+            HttpContext.Session.SetString("LoginData", JsonConvert.SerializeObject(requestModel));
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ MPKDotNetCore.LoginWebApp/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MPKDotNetCore.LoginWebApp.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
